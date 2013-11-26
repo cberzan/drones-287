490168bda7a5971a2c201ebf32cd514022e91002
Constantin Berzan <cberzan@gmail.com>
2013-11-25 17:16:41 -0800

    wip code for pose-estimation geometry

--- vision-code/PoseEstimator/Geometry.h
+++ vision-code/PoseEstimator/Geometry.h
@@ -12,15 +12,12 @@ using namespace cv;
  * worldPts (Nx4) = world points in homogeneous coordinates
  * imagePts (Nx3) = image points in homogeneous coordinates
  *
- * Outputs:
- * rotation (3x3) = rotation of camera w.r.t. world
- * translation (3x1) = translation of camera w.r.t. world
+ * Returns:
+ * pose (3x4) = [3x3] rotation and [3x1] translation of camera w.r.t. world
  */
-void estimateRotationTranslation(
-    Mat const worldPts,
-    Mat const imagePts,
-    Mat rotation,
-    Mat translation);
+Mat_<double> estimateRotTransl(
+    Mat_<double> const worldPtsHom,
+    Mat_<double> const imagePtsHom);
 
 /**
  * Estimate camera pose w.r.t. hardcoded landing pad.
@@ -28,7 +25,73 @@ void estimateRotationTranslation(
  * Inputs:
  * imagePts (Nx2) = image points in the order described in the paper
  *
- * Outputs:
- * pose (3x1) = x, y, z coords of camera
+ * Returns:
+ * pose (4x1) = x, y, z, yaw coords of camera
  */
-void estimatePose(Mat const imagePts, Mat pose);
+Mat_<double> estimatePose(Mat_<double> const imagePts);
+
+/**
+ * Return landing pad points in the order described in the paper.
+ *
+ * The coordinates are in millimeters.
+ * The origin of the coordinate system is point #3, the pad center.
+ *
+ * Returns:
+ * worldPts (24x4) = world points in homogeneous coordinates
+ */
+Mat_<double> getWorldPtsHom();
+
+/**
+ * Convert from Cartesian to homogeneous coordinates.
+ *
+ * Inputs:
+ * cart (NxD) = N points in D dimensions
+ *
+ * Returns:
+ * hom (NxD+1) = N points in homogeneous coordinates.
+ */
+Mat_<double> cartToHom(Mat_<double> const cart);
+
+/**
+ * Convert from homogeneous to Cartesian coordinates.
+ *
+ * Inputs:
+ * hom (NxD+1) = N points in homogeneous coordinates.
+ *
+ * Returns:
+ * cart (NxD) = N points in D dimensions
+ */
+Mat_<double> homToCart(Mat_<double> const hom);
+
+/**
+ * Compute 3D rotation matrix given rotation vector and angle.
+ *
+ * Inputs:
+ * rotAxis (3x1) = unit vector indicating rotation axis
+ * rotAngle = amount of rotation in radians
+ *
+ * Returns:
+ * rotation (3x3) = rotation matrix
+ */
+Mat_<double> rotAxisAngleToRotMatrix(
+    Mat_<double> const rotAxis,
+    double rotAngle);
+
+/**
+ * Project points in the world to points in the image.
+ *
+ * Everything is in homogeneous coordinates.
+ * We assume the camera calibration matrix is identity.
+ *
+ * Inputs:
+ * worldPtsHom (Nx4) = world points in homogeneous coordinates
+ * rotMatrix (3x3) = rotation of camera w.r.t. world
+ * translation (3x1) = translation of camera w.r.t. world
+ *
+ * Returns:
+ * imagePtsHom (Nx3) = image points in homogeneous coordinates
+ */
+Mat_<double> worldHomToCameraHom(
+    Mat_<double> const worldPtsHom,
+    Mat_<double> const rotMatrix,
+    Mat_<double> const translation);
