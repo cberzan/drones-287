e1c8b051a4502bd50d5333d683bc12ecfef12661
Sunil Shah <git@ss21k.co.uk>
2013-12-06 11:52:45 -0800

    WIP

--- code/lander/LanderStates.cpp
+++ code/lander/LanderStates.cpp
@@ -4,61 +4,55 @@
 
 #include "roscopter/RC.h"
 
-bool landerActive = false;
+namespace LanderStates {
 
-bool isLanderActive() {
-	return landerActive;
-}
+	bool landerActive = false;
 
-bool updateLanderActive(int32 controlChannelValue) {
-	bool oldLanderActive = false;
-	if (controlChannelValue >= 1000 && controlChannelValue <= 2000) //CHECK ME
-		landerActive = true;
-	else 
-		landerActive = false;
-
-	return (oldLanderActive == landerActive);
-}
-
-enum class States : char {
-	FLYING,
-	SEEK_HOME,
-	LAND_HIGH,
-	LAND_LOW,
-	POWER_OFF
-};
-
-States currentState;
-
-States getState () {
-	return currentState;
-}
-
-bool setState (States newState) {
-	bool changed = true;
-	if (newState == currentState) {
-		changed = false;
-	} 
-	else {
-		currentState = newState;
+	bool isLanderActive() {
+		return landerActive;
 	}
-	return changed;
-}
 
-roscopter::RC performStateAction() {
-	switch (currentState)
-	case (States.FLYING):
-		return Controller::getManualControlMsg();
-	case (States.SEEK_HOME):
-		return Controller::getRTLControlMsg()
-	case (States.LAND_HIGH):
-		return Controller::getTranslateAndDescendControlMsg()
-	case (States.LAND_LOW):
-		return Controller::getDescentOnlyControlMsg();
-	case (States.POWER_OFF):
-		if (QuadcopterState::onGround()) {
-			return Controller::getPowerOffControlMsg(); 
+	bool updateLanderActive(int controlChannelValue) {
+		bool oldLanderActive = false;
+		if (controlChannelValue >= 1000 && controlChannelValue <= 2000) //CHECK ME
+			landerActive = true;
+		else 
+			landerActive = false;
+
+		return (oldLanderActive == landerActive);
+	}
+
+	States currentState;
+
+	States getState () {
+		return currentState;
+	}
+
+	bool setState (States newState) {
+		bool changed = true;
+		if (newState == currentState) {
+			changed = false;
+		} 
+		else {
+			currentState = newState;
 		}
+		return changed;
+	}
+
+	roscopter::RC performStateAction() {
+		switch (currentState)
+		case (States.FLYING):
+			return Controller::getManualControlMsg();
+		case (States.SEEK_HOME):
+			return Controller::getRTLControlMsg()
+		case (States.LAND_HIGH):
+			return Controller::getTranslateAndDescendControlMsg()
+		case (States.LAND_LOW):
+			return Controller::getDescentOnlyControlMsg();
+		case (States.POWER_OFF):
+			if (QuadcopterState::onGround()) {
+				return Controller::getPowerOffControlMsg(); 
+			}
+	}
+
 }
-
-
--- code/lander/QuadcopterState.h
+++ code/lander/QuadcopterState.h
@@ -1,34 +1,61 @@
 #include "std_msgs/Float64MultiArray.h"
+#include "std_msgs/Float32.h"
+#include "std_msgs/Int16.h"
+#include <stdint.h>
 
 #include "roscopter/Attitude.h"
 #include "roscopter/VFR_HUD.h"
 
-struct QuadcopterState;
-struct QuadcopterPose;
+namespace QuadcopterState {
 
-QuadcopterState latestState;
-QuadcopterPose latestPose;
+	struct QuadcopterState {
+		float32_t roll;
+		float32_t pitch;
+		float32_t yaw; 
+		float32_t airspeed;
+		float32_t groundspeed; 
+		int16_t heading;
+		uint16_t throttle;
+		float32_t alt;
+		float32_t climb;
+	};
 
-/**
-* Updates the latest attitude values from the autopilot
-* 
-* Input:
-* attitudeMsg = Attitude message posted by roscopter
-*/
-void updateAttitude(const roscopter::Attitude::ConstPtr& attitudeMsg);
 
-/**
-* Updates the latest telemetry values from the autopilot
-* 
-* Input:
-* hudMsg = VFR_HUD message posted by roscopter
-*/
-void updateTelemetry (const roscopter::VFR_HUD::ConstPtr& hudMsg);
+	struct QuadcopterPose {
+		float32 x;
+		float32 y;
+		float32 z;
+		float32 yaw;
+	};
 
-/**
-* Updates the latest pose values from the pose_estimator
-* 
-* Input:
-* poseMsg = simplePose message posted by pose_estimator
-*/
-void updatePose (const std_msgs::Float64MultiArray::ConstPtr& poseMsg);
+	/**
+	* Updates the latest attitude values from the autopilot
+	* 
+	* Input:
+	* attitudeMsg = Attitude message posted by roscopter
+	*/
+	void updateAttitude(const roscopter::Attitude::ConstPtr& attitudeMsg);
+
+	/**
+	* Updates the latest telemetry values from the autopilot
+	* 
+	* Input:
+	* hudMsg = VFR_HUD message posted by roscopter
+	*/
+	void updateTelemetry (const roscopter::VFR_HUD::ConstPtr& hudMsg);
+
+	/**
+	* Updates the latest pose values from the pose_estimator
+	* 
+	* Input:
+	* poseMsg = simplePose message posted by pose_estimator
+	*/
+	void updatePose (const std_msgs::Float64MultiArray::ConstPtr& poseMsg);
+
+
+	bool onGround();
+
+	QuadcopterState getQuadcopterState();
+
+	QuadcopterPose getQuadcopterPose();
+}
