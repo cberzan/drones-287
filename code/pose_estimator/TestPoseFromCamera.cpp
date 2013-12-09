9f57e6b9d79132cf069b7c6d5f0a703e5afeeea5
Constantin Berzan <cberzan@gmail.com>
2013-12-08 17:52:56 -0800

    add --record option to TestPoseFromCamera

--- code/pose_estimator/TestPoseFromCamera.cpp
+++ code/pose_estimator/TestPoseFromCamera.cpp
@@ -7,8 +7,13 @@ using namespace std;
 #define ESCAPE_KEY 27
 
 
-int main()
+int main(int argc, char *argv[])
 {
+    bool record = false;
+    if(argc == 2 && strcmp(argv[1], "--record") == 0) {
+        record = true;
+    }
+
     char const* inputWindowHandle = "Input";
     char const* cannyWindowHandle = "Canny";
     char const* contourWindowHandle = "Contours";
@@ -32,6 +37,7 @@ int main()
     system("v4l2-ctl -d 0 -c focus_auto=0");
     system("v4l2-ctl -d 0 -c focus_absolute=0");
 
+    int frameNo = 0;
     while(true) {
         capture >> frame;
         Mat_<double> corners = detectCorners(
@@ -45,6 +51,16 @@ int main()
             cout << simplePose << endl;
         } else {
             cout << "could not detect all corners" << endl;
+        }
+
+        if(record) {
+            stringstream s;
+            s.width(10);
+            s.fill('0');
+            s << frameNo << ".jpg";
+            imwrite(s.str(), frame);
+            cout << "wrote " << s.str() << endl;
+            frameNo++;
         }
 
         if ((char)waitKey(100) == ESCAPE_KEY)
